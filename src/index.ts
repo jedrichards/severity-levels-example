19180e47076cf95dff9eaceb0fbb246053062095
jedrichards <jedrichards@gmail.com>
2020-04-29 13:55:44 +0100

    feat: add rules

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,47 @@
-import type { Assistant } from '@sketch-hq/sketch-assistant-types'
+import { Assistant, RuleDefinition, ViolationSeverity } from '@sketch-hq/sketch-assistant-types'
 
-import { helloWorldRule } from './rules/hello-world'
+const errorRule: RuleDefinition = {
+  rule: async (context) => {
+    context.utils.report({
+      message: 'Uh oh, something definitely looks wrong here',
+    })
+  },
+  name: 'severity-levels-example/error',
+  title: 'Error violation',
+  description: 'Emits an error violation',
+}
+
+const warningRule: RuleDefinition = {
+  rule: async (context) => {
+    context.utils.report({
+      message: "This isn't critical but it might need your attention",
+    })
+  },
+  name: 'severity-levels-example/warning',
+  title: 'Warning violation',
+  description: 'Emits a warning violation',
+}
+
+const infoRule: RuleDefinition = {
+  rule: async (context) => {
+    context.utils.report({
+      message: 'This message is of purely informational value, no action required',
+    })
+  },
+  name: 'severity-levels-example/info',
+  title: 'Info violation',
+  description: 'Emits an info violation',
+}
 
 const assistant: Assistant = async () => {
   return {
-    name: 'sketch-assistant-template',
-    rules: [helloWorldRule],
+    name: 'severity-levels-example',
+    rules: [errorRule, warningRule, infoRule],
     config: {
       rules: {
-        [helloWorldRule.name]: { active: true },
+        [errorRule.name]: { active: true, severity: ViolationSeverity.error },
+        [warningRule.name]: { active: true, severity: ViolationSeverity.warn },
+        [infoRule.name]: { active: true, severity: ViolationSeverity.info },
       },
     },
   }
